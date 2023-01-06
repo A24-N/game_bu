ef2f5d8c6935130dd99652c42c6999072d33365e
A24-N <117143210+A24-N@users.noreply.github.com>
2023-01-06 06:45:09 +0000

    [create]controller

---
+++ app/controllers/introduces_controller.rb
@@ -0,0 +1,10 @@
+class IntroducesController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/show'
+  get 'posts/edit'
+  get 'introduces/index'
+  get 'introduces/new'
+  get 'introduces/edit'
+  get 'rooms/show'
+  get 'matches/index'
+  get 'users/my_page'
+  get 'users/edit'
+  get 'users/show'
+  get 'homes/top'
+  get 'homes/about'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
