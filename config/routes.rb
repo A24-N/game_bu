b6dc52a88e36990bff9ee6b9ec9599523929c9ef
A24-N <117143210+A24-N@users.noreply.github.com>
2023-01-06 08:12:06 +0000

    [add]routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,29 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/show'
-  get 'posts/edit'
-  get 'introduces/index'
-  get 'introduces/new'
-  get 'introduces/edit'
   get 'rooms/show'
   get 'matches/index'
-  get 'users/my_page'
-  get 'users/edit'
-  get 'users/show'
-  get 'homes/top'
-  get 'homes/about'
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: "homes#top"
+  get 'homes/about' => "homes#about"
+
+  resource :users, only: [] do
+    get 'my_page' => "users#my_page"
+    get 'information/edit' => "users#edit"
+    patch 'information' => "users#update"
+    get 'unsubscribe' => "users#unsubscribe"
+    patch 'withdrawal' => "users#withdrawal"
+  end
+
+  resources :users, only: [:show] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+    resource :introduces, only: [:index, :new, :create, :edit, :update, :destroy]
+  end
+
+  resources :posts, only: [:index, :create, :edit, :update, :destroy, :show] do
+    resource :favorites, only: [:create, :destroy]
+    resources :comments, only: [:create, :destroy]
+  end
+
 end
