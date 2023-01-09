99039f80c930b442fd7a2c317a17570db43ede84
A24-N <117143210+A24-N@users.noreply.github.com>
2023-01-09 05:40:40 +0000

    [add]withdrawal

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,8 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get 'about' => "homes#about"
 
-  resources :users, only: [:show, :edit, :update] do
+  resources :users, only: [:show, :edit, :update, :destroy] do
     get 'unsubscribe' => "users#unsubscribe"
-    patch 'withdrawal' => "users#withdrawal"
     resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
