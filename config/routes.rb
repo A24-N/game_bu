1866bd468ddd91b35e1bb05ea944e0f33228611d
A24-N <117143210+A24-N@users.noreply.github.com>
2023-01-14 07:08:47 +0000

    [add]admin_posts/introduces/rooms

--- config/routes.rb
+++ config/routes.rb
@@ -29,11 +29,9 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'homes#top'
 
-    resources :rooms, only: [:index]
-    resources :posts, only: [:index]
-    resources :users, only: [:index, :show, :edit, :update, :destroy] do
-      patch 'status_change' => "users#status_change"
-    end
-    resources :introduces, only: [:index]
+    resources :rooms, only: [:index, :show, :destroy]
+    resources :posts, only: [:index, :show, :destroy]
+    resources :users, only: [:index, :show, :edit, :update, :destroy]
+    resources :introduces, only: [:index, :show, :destroy]
   end
 end
