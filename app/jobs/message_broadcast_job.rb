b8b29b72f611dc60f9dce2d70e365f0b64951b14
A24-N <117143210+A24-N@users.noreply.github.com>
2023-01-19 07:48:04 +0000

    [layout]room_chat

--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -1,16 +1,13 @@
 class MessageBroadcastJob < ApplicationJob
   # queue_as :default
   def perform(message)
-    room = Room.find(message.room_id)
-    owner = room.owner
-    member = room.member
     current_user = User.find(message.user_id)
-    ActionCable.server.broadcast "room_channel_#{message.room_id}", {message: render_message(message, owner, member, room, current_user)}
+    ActionCable.server.broadcast "room_channel_#{message.room_id}", {message: render_message(message, current_user)}
   end
 
   private
 
-    def render_message(message, owner, member, room, current_user)
-      ApplicationController.renderer.render partial: 'messages/message', locals: { message: message, room: room, owner: owner, member: member, current_user: current_user }
+    def render_message(message, current_user)
+      ApplicationController.renderer.render partial: 'messages/message', locals: { message: message, current_user: current_user }
     end
 end
