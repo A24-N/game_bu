e09518506f942ca2f4c45e5b8aca962db5374486
A24-N <117143210+A24-N@users.noreply.github.com>
2023-01-19 05:05:49 +0000

    [layout]match

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -12,15 +12,18 @@ class MatchesController < ApplicationController
     end
   end
 
-#マッチングステータスをstand_byに変更
+#マッチング待機状態へ
   def create
     @match = Match.new(match_params)
     @match.user_id = current_user.id
-    @match.save
-    redirect_to request.referer
+    if @match.save
+      redirect_to request.referer, notice: "マッチング待機中です:)"
+    else
+      redirect_to request.referer, alert: "入力情報を確認してください:<"
+    end
   end
 
-#マッチングステータスをafkに変更
+#マッチグ待機状態を解除
   def destroy
     match = Match.find_by(user_id: current_user.id)
     match.destroy
@@ -29,28 +32,42 @@ class MatchesController < ApplicationController
 
 #マッチング後にルームを作成
   def matching
-    @room = Room.new
-    @room.owner_id = params[:user_id]
-    @room.member_id = current_user.id
-    @room.save
+    @match = Match.find_by(user_id: current_user.id)
+    room_check = Room.where(owner_id: params[:user_id]).or(Room.where(member_id: params[:user_id]))
+    room_check2 = Room.where(owner_id: current_user.id).or(Room.where(member_id: current_user.id))
+# ユーザーが既にマッチング済みかの判定
+    if room_check.blank? and room_check2.blank?
+      @room = Room.new
+      @room.owner_id = params[:user_id]
+      @room.member_id = current_user.id
+      @room.save
 
-    @match = Match.new
-    @match.matching_status = 2
-    @match.user_id = current_user.id
-    @match.room_id = @room.id
-    @match.save
+      @owner_match = Match.find_by(user_id: params[:user_id])
+      @owner_match.matching_status = 2
+      @owner_match.room_id = @room.id
+      @owner_match.update(params.permit(:matching_status))
 
-    @owner_match = Match.find_by(user_id: params[:user_id])
-    @owner_match.matching_status = 2
-    @owner_match.room_id = @room.id
-    @owner_match.update(params.permit(:matching_status))
+# マッチが作成されていない場合はマッチを
+      if @match.blank?
+        @match = Match.new
+      end
+      @match.matching_status = 2
+      @match.user_id = current_user.id
+      @match.room_id = @room.id
+      @match.room_comment = @owner_match.room_comment
+      @match.game_name = @owner_match.game_name
+      @match.game_hard = @owner_match.game_hard
+      @match.save
 
-    redirect_to room_path(@room)
+      redirect_to room_path(@room)
+    else
+      redirect_to request.referer, alert: "既に他のユーザーとマッチ中です:<"
+    end
   end
 
   private
 
   def match_params
-    params.require(:match).permit(:matching_status, :room_comment, :game_name)
+    params.require(:match).permit(:matching_status, :room_comment, :game_name, :game_hard)
   end
 end
