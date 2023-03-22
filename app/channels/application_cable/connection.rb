8c8eeca14e92434423804949ab667480d1a8233d
A24-N <117143210+A24-N@users.noreply.github.com>
2023-03-22 09:09:13 +0000

    [fix]ActionCable_warden_error

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -10,13 +10,13 @@ module ApplicationCable
     protected
 
     def find_verified_user
-
-      if verified_user = User.find_by(id: env['warden'].user.id)
-        verified_user
-      else
-        reject_unauthorized_connection
+      unless env['warden'].user == nil
+        if verified_user = User.find_by(id: env['warden'].user.id)
+          verified_user
+        else
+          reject_unauthorized_connection
+        end
       end
-
     end
   end
 end
